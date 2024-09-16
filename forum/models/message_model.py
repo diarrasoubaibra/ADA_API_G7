c8d0f5b380f176f79f3c3fb35f8d6a8d4df4b5ab
Diarrassouba ibrahim <diarrassoubaibra58@gmail.com>
2024-09-16 11:49:32 +0000

    modele update

--- forum/models/message_model.py
+++ forum/models/message_model.py
@@ -2,7 +2,7 @@ from django.db import models
 from forum.models.subject_model import SubjectModel
 
 class MessageModel(models.Model):
-    subjecct = models.ForeignKey(SubjectModel, related_name='subjects', on_delete=models.CASCADE)
+    subject = models.ForeignKey(SubjectModel, related_name='subjects', on_delete=models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
